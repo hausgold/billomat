9e29921a74a77ceff023a2046dddfdb1b7e72816
Marco Metz <marco.metz@gmail.com>
2021-10-11 11:43:18 +0200

    Extend models with credit_notes and items. (#14)
    To create credit note in billomat 2 more models are needed in the gem:
    
    - credit_note
    - credit_note_item
    
    Usage is similar to invoice and invoice_item.

---
+++ spec/models/credit_note_item_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe Billomat::Models::CreditNoteItem do
+  it 'has a base path' do
+    expect(described_class.base_path).to be
+  end
+
+  it 'has a resource name' do
+    expect(described_class.resource_name).to be
+  end
+end
