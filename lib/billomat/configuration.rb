ca292cc4c6645935e58c8486655a522bc82aa4d0
Stefan Schüßler <mail@stefanschuessler.de>
2023-11-06 10:06:25 +0100

    Add after_response callback (#22)

--- lib/billomat/configuration.rb
+++ lib/billomat/configuration.rb
@@ -4,5 +4,17 @@ module Billomat
   # The +billomat+ gem configuration.
   class Configuration
     attr_accessor :api_key, :subdomain, :timeout, :app_id, :app_secret
+    attr_reader :after_response
+
+    # Sets a callback to be called for each API response
+    #
+    # @param [Proc] callback The callback
+    def after_response=(callback)
+      unless callback.respond_to?(:call)
+        raise ArgumentError, "callback must respond to `call'"
+      end
+
+      @after_response = callback
+    end
   end
 end
