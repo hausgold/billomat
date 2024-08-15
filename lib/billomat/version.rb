bc16a18d8f059dbb6cfedcf29179222254f2deea
deployhausgold <deploy@hausgold.de>
2024-08-15 12:56:28 +0000

    [Release] Bumped the version to 1.2.5.

--- lib/billomat/version.rb
+++ lib/billomat/version.rb
@@ -3,7 +3,7 @@
 # The gem version details.
 module Billomat
   # The version of the +billomat+ gem
-  VERSION = '1.2.4'
+  VERSION = '1.2.5'
 
   class << self
     # Returns the version of gem as a string.
